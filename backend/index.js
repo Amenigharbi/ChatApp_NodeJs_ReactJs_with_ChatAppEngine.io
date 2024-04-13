805807fc21b2cbde4dbf8c61b915dca2d83652c4
ameni <gharbiameni17@gmail.com>
2024-04-13 18:20:52 +0100

    commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "4e4d6554-5629-49bd-8190-4b40afdf4f96" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    } else {
+      return res.status(500).json({ error: 'Une erreur est survenue lors du traitement de la requête.' });
+    }
+  }
+});
+
+app.listen(3001);
